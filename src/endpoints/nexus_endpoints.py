3b2e6ae2cd8450cfff65cf14df550e9e38450898
Rune Aagaard Keena <rune.aagaard.keena@randers.dk>
2024-10-21 10:55:09 +0200

    remved all jobs

--- src/endpoints/nexus_endpoints.py
+++ src/endpoints/nexus_endpoints.py
@@ -4,7 +4,6 @@ import logging
 from nexus.nexus_client import NexusClient
 from flask import Blueprint, request, jsonify
 from utils.config import NEXUS_CLIENT_ID, NEXUS_CLIENT_SECRET, NEXUS_URL
-from jobs.nexus_flow_lukning import execute_lukning
 
 logger = logging.getLogger(__name__)
 nexus_client = NexusClient(NEXUS_CLIENT_ID, NEXUS_CLIENT_SECRET, NEXUS_URL)
@@ -39,17 +38,3 @@ def _fetch_lendings(cpr):
         return []
 
     return patient_lendings
-
-
-@api_nexus_bp.route('/execute-lukning', methods=['POST'])
-def _execute_lukning():
-    data = request.get_json()
-    cpr = data.get('cpr')
-    if not cpr:
-        return jsonify({"error": "CPR is required"}), 400
-    try:
-        lukning = execute_lukning(cpr)
-        return jsonify(lukning), 200
-    except Exception as e:
-        logger.error(f"Failed to execute lukning: {e}")
-        return jsonify({"error": "An error occurred while executing lukning"}), 500
