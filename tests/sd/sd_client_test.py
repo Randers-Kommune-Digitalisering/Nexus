291d25b91af76300e689a5a0b8514c59daeb8248
st-randers <soren.langbo.sloto.thomassen@randers.dk>
2024-12-17 10:59:57 +0100

    Added pytests to expand coverage

--- tests/sd/sd_client_test.py
+++ tests/sd/sd_client_test.py
@@ -1,10 +1,10 @@
 import pytest
 from unittest.mock import patch
 from sd.sd_client import SDClient, SDAPIClient
-from utils.config import SD_URL, SD_USERNAME, SD_PASSWORD
-
+from utils.config import SD_URL, SD_USERNAME, SD_PASSWORD, SD_BASIC_AUTH
 
 sd_url = SD_URL or "https://sd-mock.com"
+sd_auth = SD_BASIC_AUTH or "sd-test-basic-auth"
 
 
 # SDAPIClient
@@ -70,6 +70,14 @@ def test_make_request_failure(sd_api_client, requests_mock):
     assert response is None
 
 
+def test_SDAPIClient_get_auth_headers(sd_api_client):
+    headers = sd_api_client.get_auth_headers()
+    assert headers == {
+        "authorization": "Basic " + sd_auth,
+        "content-type": "application/xml"
+    }
+
+
 # SDClient
 @pytest.fixture
 def sd_client():
@@ -113,3 +121,75 @@ def test_get_employment_changed(sd_client, requests_mock):
             response = sd_client.get_employment_changed(cpr="123456-7890", employment_identifier="emp_id", inst_code="inst_code", activation_date="01.01.2020", deactivation_date="01.01.2021")
 
     assert response == {"changes": "some changes"}
+
+
+def test_SDClient_post_request(sd_client, requests_mock):
+    path = "test"
+    url = sd_url + "/" + path
+    mock_response = "<response><result>success</result></response>"
+    requests_mock.post(url, text=mock_response)
+
+    with patch.object(SDAPIClient, 'get_auth_headers', return_value={"Authorization": "Basic test_token"}):
+        response = sd_client.post_request(path=path, json={"data": "value"})
+
+    assert response == {"response": {"result": "success"}}
+
+
+def test_SDClient_put_request(sd_client, requests_mock):
+    path = "test"
+    url = sd_url + "/" + path
+    mock_response = "<response><result>updated</result></response>"
+    requests_mock.put(url, text=mock_response)
+
+    with patch.object(SDAPIClient, 'get_auth_headers', return_value={"Authorization": "Basic test_token"}):
+        response = sd_client.put_request(path=path, json={"data": "value"})
+
+    assert response == {"response": {"result": "updated"}}
+
+
+def test_SDClient_delete_request(sd_client, requests_mock):
+    path = "test"
+    url = sd_url + "/" + path
+    mock_response = "<response><result>deleted</result></response>"
+    requests_mock.delete(url, text=mock_response)
+
+    with patch.object(SDAPIClient, 'get_auth_headers', return_value={"Authorization": "Basic test_token"}):
+        response = sd_client.delete_request(path=path)
+
+    assert response == {"response": {"result": "deleted"}}
+
+
+def test_SDClient_get_person_no_response(sd_client):
+    with patch.object(SDClient, 'get_request', return_value=None):
+        response = sd_client.get_person(cpr="123456-7890", employement_identifier="emp_id", inst_code="inst_code")
+    assert response is False
+
+
+def test_SDClient_get_person_no_person_data(sd_client):
+    with patch.object(SDClient, 'get_request', return_value={"GetPerson": {}}):
+        response = sd_client.get_person(cpr="123456-7890", employement_identifier="emp_id", inst_code="inst_code")
+    assert response is None
+
+
+def test_SDClient_get_employment_no_response(sd_client):
+    with patch.object(SDClient, 'get_request', return_value=None):
+        response = sd_client.get_employment(cpr="123456-7890", employment_identifier="emp_id", inst_code="inst_code")
+    assert response is False
+
+
+def test_SDClient_get_employment_no_employment_data(sd_client):
+    with patch.object(SDClient, 'get_request', return_value={"GetEmployment20070401": {"Person": {}}}):
+        response = sd_client.get_employment(cpr="123456-7890", employment_identifier="emp_id", inst_code="inst_code")
+    assert response is None
+
+
+def test_SDClient_get_employment_changed_no_response(sd_client):
+    with patch.object(SDClient, 'get_request', return_value=None):
+        response = sd_client.get_employment_changed(cpr="123456-7890", employment_identifier="emp_id", inst_code="inst_code", activation_date="01.01.2020", deactivation_date="01.01.2021")
+    assert response is False
+
+
+def test_SDClient_get_employment_changed_no_person_data(sd_client):
+    with patch.object(SDClient, 'get_request', return_value={"GetEmploymentChanged20070401": {}}):
+        response = sd_client.get_employment_changed(cpr="123456-7890", employment_identifier="emp_id", inst_code="inst_code", activation_date="01.01.2020", deactivation_date="01.01.2021")
+    assert response is None
